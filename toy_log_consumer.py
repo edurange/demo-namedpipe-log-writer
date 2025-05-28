853613845f8a168fce9dd07e62991ab2793926fc
Joseph Granville <jwgranville>
2025-05-28 07:37:51 -0700

    Correct some conventions

--- toy_log_consumer.py
+++ toy_log_consumer.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
-import asyncio
-from contextlib import asynccontextmanager
-from datetime import datetime
-import sys
-import time
-
 """
 A file reader to contrast the features of files and named pipes
 
 Uses a thread to wrap the blocking IO in an awaitable and release the
 Global Interpreter Lock. This is not a general endorsement of threading.
 """
+
+import asyncio
+from contextlib import asynccontextmanager
+from datetime import datetime
+import sys
+import time
 
 __author__ = "Joe Granville"
 __date__ = "20250526"
@@ -27,24 +27,24 @@ async def asyncopen(path, mode):
     Asynchronously waits for a file to open, then closes it
     """
     print(f"Opening {path}")
-    file = await asyncio.to_thread(open, path, mode)
+    file = await asyncio.to_thread(open, path, mode, encoding="utf-8")
     yield file
     print(f"Closing {path}")
     file.close()
 
 
-async def coroutineread(filepath):
+async def coroutineread(path):
     """
     Asynchronously reads and prints lines as they become available
     """
-    async with asyncopen(filepath, "r") as file:
-        print(f'I opened "{filepath}" to read')
+    async with asyncopen(path, "r") as file:
+        print(f'I opened "{path}" to read')
         while True:
             print("I will not poll the filesystem")
             print(f"It's {datetime.now()} and I'm waiting to read")
             line = await asyncio.to_thread(file.readline)
             print(f'The producer wrote "{line.strip()}"')
-            
+
 
 DELAY = 1
 
@@ -57,19 +57,19 @@ async def busywork():
         print(f"It's {datetime.now()}")
         print(f"I get other things done every {DELAY} seconds")
         time.sleep(DELAY)
-            
-            
-async def tasks(filepath):
+
+
+async def tasks(path):
     """
     Runs coroutineread and busywork together concurrently
     """
-    asyncio.gather(coroutineread(filepath), busywork())
-            
-            
+    asyncio.gather(coroutineread(path), busywork())
+
+
 if __name__ == "__main__":
     filepath = sys.argv[1]
     print(
         'Teacher made me write "I will not poll the filesystem" an '
-        'indefinite number of times'
+        "indefinite number of times"
     )
     asyncio.run(tasks(filepath))
--- toy_log_producer.py
+++ toy_log_producer.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
-from datetime import datetime
-import sys
-import time
-
 """
 A simple continuous file writer
 
 Uses time.sleep() to simulate work that is CPU-bound.
 """
+
+from datetime import datetime
+import sys
+import time
 
 __author__ = "Joe Granville"
 __date__ = "20250526"
@@ -22,7 +22,7 @@ DELAY = 3
 if __name__ == "__main__":
     filepath = sys.argv[1]
     print("I'll write whenever I want, watch me")
-    with open(filepath, "a") as file:
+    with open(filepath, "a", encoding="utf-8") as file:
         print(f'I opened "{filepath}" to append')
         while True:
             print(f"I wrote at {datetime.now()}")
